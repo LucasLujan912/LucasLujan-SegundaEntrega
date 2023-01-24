b415610ca4e3e47d649855663880ac438ba26be8
Lucas Lujan <lucaslujann@gmail.com>
2023-01-24 18:22:46 -0300

    REPOSITORIO LUCASLUJAN-SEGUNDA ENTREGA

---
+++ script.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu');
+
+let menu__bar = document.querySelector('#menu__bar');
+
+    menu__bar.addEventListener('click',function() {
+  
+    menu.classList.toggle('menu__toggle')
+
+   })
